846e9d15fe7f6e6f3215e3f30ef255d71f0aaa4b
space-commits <57108281+space-commits@users.noreply.github.com>
2023-01-06 18:46:00 +0000

    Config, Optic FOV, Camera Position, ADS FOV
    The Optic's internal FOV, ADS FOV, and Camera Position Relative to Optic can now all be changed.

---
+++ Helper.cs
@@ -0,0 +1,110 @@
+﻿using EFT.InventoryLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using UnityEngine;
+
+namespace FOVFix
+{
+  public static class Helper
+  {
+
+        public static string CompactCollimator = "55818acf4bdc2dde698b456b";
+        public static string Collimator = "55818ad54bdc2ddc698b4569";
+        public static string AssaultScope = "55818add4bdc2d5b648b456f";
+        public static string Scope = "55818ae44bdc2dde698b456c";
+        public static string IronSight = "55818ac54bdc2d5b648b456e";
+        public static string SpecialScope = "55818aeb4bdc2ddc698b456a";
+
+        public static string[] scopeTypes = new string[] { "55818acf4bdc2dde698b456b", "55818ad54bdc2ddc698b4569", "55818add4bdc2d5b648b456f", "55818ae44bdc2dde698b456c", "55818ac54bdc2d5b648b456e", "55818aeb4bdc2ddc698b456a" };
+
+
+
+        public static bool IsSight(Mod mod)
+        {
+            bool isScope = false;
+
+            foreach (string id in scopeTypes) 
+            {
+                isScope = mod.GetType() == TemplateIdToObjectMappingsClass.TypeTable[id] ? true : false;
+            }
+
+            return isScope;
+        }
+
+        public static float getADSFoVMulti(float zoom) 
+        {
+            switch (zoom)
+            {
+                case <= 1:
+                    return Plugin.oneADSMulti.Value;
+                case <= 2:
+                    return Plugin.twoADSMulti.Value;
+                case <= 3:
+                    return Plugin.threeADSMulti.Value;
+                case <= 4:
+                    return Plugin.fourADSMulti.Value;
+                case <= 6:
+                    return Plugin.sixADSMulti.Value;
+                case <=12:
+                    return Plugin.twelveADSMulti.Value;
+                case > 12:
+                    return Plugin.highADSMulti.Value;
+                default:
+                    return 1;
+            }
+        }
+
+        public static float getCameraPosMulti(float zoom)
+        {
+            switch (zoom)
+            {
+                case <= 1:
+                    return Plugin.oneCameraPOSMulti.Value;
+                case <= 2:
+                    return Plugin.twoCameraPOSMulti.Value;
+                case <= 3:
+                    return Plugin.threeCameraPOSMulti.Value;
+                case <= 4:
+                    return Plugin.fourCameraPOSMulti.Value;
+                case <= 6:
+                    return Plugin.sixCameraPOSMulti.Value;
+                case <= 12:
+                    return Plugin.twelveCameraPOSMulti.Value;
+                case > 12:
+                    return Plugin.highCameraPOSMulti.Value;
+                default:
+                    return 1;
+            }
+        }
+/*
+        public static float getOpticFOVMulti(float zoom)
+        {
+            switch (zoom)
+            {
+                case <= 1:
+                    return Plugin.oneOpticFOVMulti.Value;
+                case <= 2:
+                    return Plugin.twoOpticFOVMulti.Value;
+                case <= 3:
+                    return Plugin.threeOpticFOVMulti.Value;
+                case <= 4:
+                    return Plugin.fourOpticFOVMulti.Value;
+                case <= 6:
+                    return Plugin.sixOpticFOVMulti.Value;
+                case <= 12:
+                    return Plugin.twelveOpticFOVMulti.Value;
+                case > 12:
+                    return Plugin.highOpticFOVMulti.Value;
+                default:
+                    return 1;
+            }
+        }*/
+
+
+
+
+
+    }
+}
--- Patches.cs
+++ Patches.cs
@@ -1,6 +1,8 @@
 ﻿using Aki.Reflection.Patching;
 using Comfort.Common;
 using EFT;
+using EFT.Animations;
+using EFT.InventoryLogic;
 using HarmonyLib;
 using System.Collections;
 using System.Reflection;
@@ -8,7 +10,83 @@ using UnityEngine;
 
 namespace FOVFix
 {
-    public class SetFovPatch : ModulePatch
+
+    //this gets called when a new scene is created, so can't change fieldofview multi by current zoom level. 
+    public class OpticSightAwakePatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(EFT.CameraControl.OpticSight).GetMethod("Awake", BindingFlags.Instance | BindingFlags.Public);
+        }
+
+        [PatchPrefix]
+        private static bool Prefix(EFT.CameraControl.OpticSight __instance)
+        {
+            __instance.TemplateCamera.gameObject.SetActive(false);
+            __instance.TemplateCamera.fieldOfView *= Plugin.globalOpticFOVMulti.Value;
+            return false;
+        }
+    }
+
+    public class GetAnyOpticsDistanceToCameraPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(ScopePrefabCache).GetMethod("GetAnyOpticsDistanceToCamera", BindingFlags.Instance | BindingFlags.Public);
+        }
+
+        [PatchPrefix]
+        private static bool Prefix(ScopePrefabCache __instance, ref float __result)
+        {
+            Logger.LogWarning("///////////////////////////////////////////////");
+            Logger.LogWarning("GetAnyOpticsDistanceToCamera");
+            ScopePrefabCache.ScopeModeInfo[] _scopeModeInfos = (ScopePrefabCache.ScopeModeInfo[])AccessTools.Field(typeof(ScopePrefabCache), "_scopeModeInfos").GetValue(__instance);
+            float distanceMulti = Helper.getCameraPosMulti(Plugin.currentZoomLevel) * Plugin.globalCameraPOSMulti.Value;
+            Logger.LogWarning("Zoom = " + Plugin.currentZoomLevel);
+            Logger.LogWarning("Zoom Multied = " + distanceMulti);
+            Logger.LogWarning("///////////////////////////////////////////////");
+            if (_scopeModeInfos[__instance.CurrentModeId].OpticSight != null)
+            {
+                __result = _scopeModeInfos[__instance.CurrentModeId].OpticSight.DistanceToCamera * distanceMulti;
+                return false;
+            }
+            __result = __instance.FirstOptic.DistanceToCamera * distanceMulti;
+            return false;
+        }
+    }
+
+    public class CalcDistancePatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(EFT.CameraControl.OpticSight).GetMethod("CalcDistance", BindingFlags.Instance | BindingFlags.Public);
+        }
+
+        [PatchPrefix]
+        private static bool Prefix(EFT.CameraControl.OpticSight __instance)
+        {
+            Logger.LogWarning("================================================CalcDistancePatch==================================================");
+
+            float distanceMulti = Helper.getCameraPosMulti(Plugin.currentZoomLevel) * Plugin.globalCameraPOSMulti.Value;
+
+            if (__instance.ScopeTransform != null)
+            {
+                __instance.DistanceToCamera = Vector3.Distance(__instance.ScopeTransform.position * distanceMulti, __instance.LensRenderer.transform.position * distanceMulti);
+                return false;
+            }
+            Transform transform = __instance.transform.Find("mod_aim_camera");
+            if (transform == null)
+            {
+                Debug.Log("Cant set distance for " + __instance.name);
+                return false;
+            }
+            __instance.DistanceToCamera = Vector3.Distance(transform.position * distanceMulti, __instance.LensRenderer.transform.position * distanceMulti);
+            return false;
+        }
+    }
+
+    //better to do it in method_17Patch, as this method also sets FOV in general.
+/*    public class SetFovPatch : ModulePatch
     {
         protected override MethodBase GetTargetMethod()
         {
@@ -16,11 +94,11 @@ namespace FOVFix
         }
 
         [PatchPrefix]
-        private static bool Prefix(CameraClass __instance, float time, Coroutine ___coroutine_0, bool applyFovOnCamera = true)
+        private static bool Prefix(CameraClass __instance, ref float x, float time, Coroutine ___coroutine_0, bool applyFovOnCamera = true)
         {
 
             var _method_4 = AccessTools.Method(typeof(CameraClass), "method_4");
-            float fov = Singleton<GClass1659>.Instance.Game.Settings.FieldOfView.Value;
+            float fov = x * Plugin.globalADSMulti.Value;
 
             if (___coroutine_0 != null)
             {
@@ -35,5 +113,58 @@ namespace FOVFix
             ___coroutine_0 = StaticManager.BeginCoroutine(meth4Enumer);
             return false;
         }
+    }*/
+
+    public class method_17Patch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(EFT.Animations.ProceduralWeaponAnimation).GetMethod("method_17", BindingFlags.Instance | BindingFlags.NonPublic);
+        }
+
+        [PatchPostfix]
+        private static void PatchPostfix(ref EFT.Animations.ProceduralWeaponAnimation __instance)
+        {
+            Logger.LogWarning("=======================================");
+            Logger.LogWarning("method_17Patch");
+            Player.FirearmController firearmController = (Player.FirearmController)AccessTools.Field(typeof(EFT.Animations.ProceduralWeaponAnimation), "firearmController_0").GetValue(__instance);
+
+            if (firearmController != null)
+            {
+                Player player = (Player)AccessTools.Field(typeof(EFT.Player.FirearmController), "_player").GetValue(firearmController);
+                if (!player.IsAI)
+                {
+                    if (__instance.PointOfView == EPointOfView.FirstPerson)
+                    {
+                        int AimIndex = (int)AccessTools.Property(typeof(EFT.Animations.ProceduralWeaponAnimation), "AimIndex").GetValue(__instance);
+                       
+                        if (!__instance.Sprint && AimIndex < __instance.ScopeAimTransforms.Count)
+                        {
+                            bool bool_1 = (bool)AccessTools.Field(typeof(EFT.Animations.ProceduralWeaponAnimation), "bool_1").GetValue(__instance);
+                            float Single_2 = (float)AccessTools.Property(typeof(EFT.Animations.ProceduralWeaponAnimation), "Single_2").GetValue(__instance);
+
+                            float zoom = 1;
+                            if (player.ProceduralWeaponAnimation.CurrentAimingMod != null) 
+                            {
+                                zoom = player.ProceduralWeaponAnimation.CurrentAimingMod.GetCurrentOpticZoom();
+
+                            }
+                            Plugin.currentZoomLevel = zoom;
+                            Logger.LogWarning("method 17 zoom = " + zoom);
+                            float baseFOV = Single_2;
+                            float sightFOV = baseFOV * Helper.getADSFoVMulti(zoom) * Plugin.globalADSMulti.Value;
+
+                            float fov = __instance.IsAiming ? sightFOV : baseFOV;
+                            CameraClass.Instance.SetFov(fov, 1f, !bool_1);
+                        }
+                        var method_0 = AccessTools.Method(typeof(EFT.Animations.ProceduralWeaponAnimation), "method_0");
+
+                        method_0.Invoke(__instance, new object[] { });
+                    }
+                }
+            }
+            Logger.LogWarning("=======================================");
+        }
     }
 }
+ 
