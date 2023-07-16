8e2dcbba4f41f4cd40b50e91229074590e628e1a
space-commits <57108281+space-commits@users.noreply.github.com>
2023-07-16 16:33:44 +0100

    Compatibility fixes
    Added check for geff's mod so that Elcna's can be handled correctly in either case. Changed how min/max zoom is set and read to potentially reduce compatibility issues. Lowered free look limit a bit.

--- Fontaine-FOV-Fix-Server/src/mod.js
+++ Fontaine-FOV-Fix-Server/src/mod.js
@@ -7,6 +7,14 @@ class Mod {
         const logger = container.resolve("WinstonLogger");
         const tables = container.resolve("DatabaseServer").getTables();
         const itemDB = tables.templates.items;
+        const preAkiModLoader = container.resolve("PreAkiModLoader");
+        const activeMods = preAkiModLoader.getImportedModDetails();
+        var geffModPresent = false;
+        for (const modname in activeMods) {
+            if (modname === "ACOG4Life-OpticRework") {
+                geffModPresent = true;
+            }
+        }
         for (let i in itemDB) {
             let serverItem = itemDB[i];
             if (serverItem._id === "5c07dd120db834001c39092d" || serverItem._id === "5c0a2cec0db834001b7ce47d" || serverItem._parent === "55818add4bdc2d5b648b456f" || serverItem._parent === "55818ae44bdc2dde698b456c" || serverItem._parent === "55818ae44bdc2dde698b456c" || serverItem._parent === "55818aeb4bdc2ddc698b456a") {
@@ -16,14 +24,17 @@ class Mod {
                         if (serverItem._id === "5d0a3a58d7ad1a669c15ca14" || serverItem._id === "5d0a3e8cd7ad1a6f6a3d35bd") {
                             serverItem._props.Zooms = [[fileItem.MinZoom, fileItem.MinZoom, fileItem.MaxZoom, fileItem.MaxZoom]];
                         }
-                        else if (serverItem._id === "57aca93d2459771f2c7e26db" || serverItem._id === "57ac965c24597706be5f975c" || serverItem._id === "5c0517910db83400232ffee5") {
-                            serverItem._props.Zooms = [[fileItem.MaxZoom, fileItem.MinZoom]];
-                        }
                         else if (fileItem.IsFixed == true && fileItem.CanToggle == false) {
                             serverItem._props.Zooms = [[fileItem.MinZoom]];
                         }
                         else if (serverItem._id === "5dfe6104585a0c3e995c7b82") {
                             serverItem._props.Zooms = [[fileItem.MinZoom, fileItem.MaxZoom], [1]];
+                        }
+                        else if (geffModPresent == true && (serverItem._id === "57aca93d2459771f2c7e26db" || serverItem._id === "57ac965c24597706be5f975c")) {
+                            serverItem._props.Zooms = [[fileItem.MaxZoom, fileItem.MinZoom], [1]];
+                        }
+                        else if (serverItem._props.Zooms[0][0] > serverItem._props.Zooms[0][1]) {
+                            serverItem._props.Zooms = [[fileItem.MaxZoom, fileItem.MinZoom]];
                         }
                         else {
                             serverItem._props.Zooms = [[fileItem.MinZoom, fileItem.MaxZoom]];
@@ -32,7 +43,7 @@ class Mod {
                             for (let k in serverItem._props.AimSensitivity[0]) {
                                 serverItem._props.AimSensitivity[0][k] = 0.7;
                             }
-                            if (serverItem._id === "5dfe6104585a0c3e995c7b82") {
+                            if (serverItem._id === "5dfe6104585a0c3e995c7b82" || serverItem._id === "57aca93d2459771f2c7e26db" || serverItem._id === "57ac965c24597706be5f975c") {
                                 serverItem._props.AimSensitivity[1][0] = 0.7;
                             }
                         }
