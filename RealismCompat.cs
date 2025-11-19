8f9c65b3ea967f32c13878be7bef01bcbe6e146f
spacecommits <57108281+space-commits@users.noreply.github.com>
2025-11-19 22:22:34 +0000

    Actually fully smoothed aiming + more configs

--- RealismCompat.cs
+++ RealismCompat.cs
@@ -9,8 +9,7 @@ namespace FOVFix
     {
         public bool HasShoulderContact { get; private set; } = false;
         public bool IsMachinePistol { get; private set; } = false;
-        public bool RealismAltRifle { get; private set; } = false;
-        public bool RealismAltPistol { get; private set; } = false;
+        public bool DoAltPistol { get; private set; } = false;
         public float StanceBlenderValue { get; private set; } = 0f;
         public float StanceBlenderTarget { get; private set; } = 0f;
         public bool StancesAreEnabled { get; private set; } = false;
@@ -18,22 +17,26 @@ namespace FOVFix
         public bool StopCameraMovmentForCollision { get; private set; } = false;
         public float CameraMovmentForCollisionSpeed { get; private set; } = 1f;
         public bool IsColliding { get; private set; } = false;
+        public bool IsLeftShoulder { get; private set; } = false;
+        public bool IsResettingShoulder { get; private set; } = false;
+        public bool IsFiringMovement { get; private set; } = false;
+        public bool DoAltRifle { get; private set; } = false;
 
         public void Update() 
         {
             HasShoulderContact = RealismMod.WeaponStats.HasShoulderContact;
             IsMachinePistol = RealismMod.WeaponStats.IsMachinePistol;
-            RealismAltRifle = RealismMod.PluginConfig.EnableAltRifle.Value;
-            RealismAltPistol = RealismMod.PluginConfig.EnableAltPistol.Value;
+            DoAltPistol = RealismMod.PluginConfig.EnableAltPistol.Value;
             StanceBlenderTarget = RealismMod.StanceController.StanceBlender.Target;
             StanceBlenderValue = RealismMod.StanceController.StanceBlender.Value;
             StancesAreEnabled = RealismMod.Plugin.ServerConfig.enable_stances;
             DoPatrolStanceAdsSmoothing = !RealismMod.StanceController.FinishedUnPatrolStancing;
-            DoPatrolStanceAdsSmoothing = !RealismMod.StanceController.FinishedUnPatrolStancing;
             StopCameraMovmentForCollision = RealismMod.StanceController.StopCameraMovement;
             IsColliding = RealismMod.StanceController.IsColliding;
             CameraMovmentForCollisionSpeed = RealismMod.StanceController.CameraMovmentForCollisionSpeed;
+            IsLeftShoulder = RealismMod.StanceController.IsLeftShoulder;
+            IsResettingShoulder = RealismMod.StanceController.IsLeftStanceResetState;
+            DoAltRifle = RealismMod.PluginConfig.EnableAltRifle.Value;
         }
-
     }
 }
